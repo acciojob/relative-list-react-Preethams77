5e56e2b3aebc11e471fd4ad4ec5c69489c44026f
Preethams77 <115490278+Preethams77@users.noreply.github.com>
2023-01-19 15:59:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,23 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const App = () => {
-  const[relativeListItem,setRelativeListItem]=useState(["motherinlaw","fatherinlaw","brotherinlaw","sisterinlaw","motherinlaw"]);
+  // const relatives=["Mama", "chachu","bro","sis","uncle"]
+  const [relatives, setRelatives] = useState([
+    "Mama",
+    "chachu",
+    "bro",
+    "sis",
+    "uncle",
+  ]);
   return (
     <div id="main">
       <ol id={"relativeList"}>
-        {relativeListItem.map((relativeListItem,idx)=>{
-          console.log(relativeListItem + idx);
-          return <li id={`relativeListItem${idx+1}`}>Name:{relativeListItem}</li>;
+        {relatives.map((relative, idx) => {
+          return <li id={`relativeListItem${idx+1}`}>Name: {relative}</li>;
         })}
       </ol>
-              
     </div>
   );
 };
+
 export default App;
